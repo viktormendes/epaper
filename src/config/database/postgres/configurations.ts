b4b3b607be3f00877023d9a1d74ed5dc0b49db43
viktormendes <victormendes04012004@gmail.com>
2026-01-09 14:02:19 -0300

    dev: config database infos and migrations

---
+++ src/config/database/postgres/configurations.ts
@@ -0,0 +1,17 @@
+import { DataSource } from 'typeorm';
+import 'dotenv/config';
+
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: [`dist/**/*.entity{.ts,.js}`],
+  migrations: [`dist/database/migration/{*.ts,*.js}`],
+  migrationsRun: true,
+  logging: true,
+});
+
+export default AppDataSource;
