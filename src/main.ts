243597011278734f6e6007f2da9e686e6785694c
viktormendes <victormendes04012004@gmail.com>
2026-01-08 22:26:23 -0300

    dev: enable cors and use swagger module

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+import { SwaggerModuleConfig } from './config/swagger/config.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      disableErrorMessages: false,
+    }),
+  );
+
+  app.enableCors({
+    origin: process.env.URL_FRONTEND,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  SwaggerModuleConfig.setup(app);
+
+  await app.listen(process.env.SERVER_PORT ?? 5000, '0.0.0.0');
+}
+
+void bootstrap();
