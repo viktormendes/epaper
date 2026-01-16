555dd14ee3c6f39e7bb85cd61e94715e2c4d20ce
viktormendes <victormendes04012004@gmail.com>
2026-01-16 14:42:10 -0300

    dev: adjust swagger configuration

--- src/config/swagger/configuration.ts
+++ src/config/swagger/configuration.ts
@@ -1,9 +1,12 @@
 import { DocumentBuilder } from '@nestjs/swagger';
 
 export const swaggerConfig = new DocumentBuilder()
-  .setTitle('GUIA API')
-  .setDescription(
-    'API para gerenciar disciplinas e suas informações relacionadas',
-  )
-  .setVersion('1.0')
-  .build();
+      .setTitle('E-Paper API')
+      .setDescription('API para autenticação e gerenciamento de e-papers')
+      .setVersion('1.0')
+      .addBearerAuth({
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      }, 'JWT-auth')
+      .build();
